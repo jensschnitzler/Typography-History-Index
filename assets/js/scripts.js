53b719a9f4204f90634e46a25f68e8352d7e7514
jensschnitzler <jens.schnitzler@gmail.com>
2022-01-29 15:55:53 +0100

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,26 @@
 /* My Scripts */
 
-console.log('Hello!');
+function slugify(text){
+  var slug = text.toLowerCase().replace(/ /g,'-').replace(/[-]+/g, '-').replace(/[^\w-]+/g,'');
+  return slug;
+}
+
+$(function(){
+
+  console.log('Hello!');
+
+  const myTable = $('table');
+  const myTHead = myTable.children('thead');
+  const myTBody = myTable.children('tbody');
+
+  myTHead.find('th').each(function(){
+    var th = $(this);
+    var text = th.text();
+    var slug = slugify(text);
+    th.attr('data-id',slug);
+    console.log({slug});
+  });
+
+
+
+});
