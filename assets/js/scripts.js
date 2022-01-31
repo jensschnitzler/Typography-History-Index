3ba9d1e76083ca5cedee73ebb2f751aff545bfe7
jensschnitzler <jens.schnitzler@gmail.com>
2022-01-31 10:08:35 +0100

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -72,18 +72,46 @@ $(function(){
 
   myTBody.children('tr:not(".empty-row")').each(function(i){ // loop rows
     var tr = $(this);
-    console.log(i);
+    //console.log(i);
     var start = tr.children('td[data-id="start"]').text().trim();
-    console.log({start});
+    //console.log({start});
     var end = tr.children('td[data-id="end"]').text().trim();
-    console.log({end});
+    //console.log({end});
     var title = tr.children('td[data-id="title"]').text().trim();
-    console.log({title});
+    //console.log({title});
     var desc = tr.children('td[data-id="description"]').text().trim();
-    console.log({desc});
+    //console.log({desc});
     var category = tr.children('td[data-id="category"]').text().trim();
-    console.log({category});
+    //console.log({category});
   });
+
+  // DOM element where the Timeline will be attached
+  var container = myVis[0];
+
+  // Create a DataSet (allows two way data-binding)
+  var items = new vis.DataSet([
+    {id: 1, content: 'item 1', start: new Date(2010,9,23),group: 1},
+    {id: 2, content: 'item 2', start: new Date(2011,9,23), end: new Date(2012,9,23),group: 1},
+    {id: 3, content: 'item 3', start: new Date(2013,9,23), type: 'point',group: 2}
+  ]);
+
+  // Configuration for the Timeline
+  var options = {
+    width: '100%',
+    height: '100%',
+    autoResize: true,
+    start: new Date(0,1,1),
+    end: new Date(2022,1,1),
+    //margin: { item: 20 }
+  };
+
+  var groups = [
+    {id: 1, content: 'Group 1'}, // Optional: a field 'className', 'style', 'order', [properties]
+    {id: 2, content: 'Group 2'},
+  ];
+
+  // Create a Timeline
+  var timeline = new vis.Timeline(container, items, groups, options);
 
 
 });
