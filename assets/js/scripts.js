526c4709bc3863a852cb2587d48e7a79e665d84b
jensschnitzler <jens.schnitzler@gmail.com>
2022-01-31 10:14:55 +0100

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -58,13 +58,17 @@ $(function(){
   myVisContainer.css({
     'display': 'block',
     'margin': 'auto',
-    'height': 'auto',
+    'width': '100vw',
+    'max-width': '100%',
+    'height': '100vh',
   });
 
   var width = Math.floor( window.innerWidth ); // size canvas width according to viewport width! redraw again on resize.
   var height = Math.floor( window.innerHeight );
 
-  /* --- Initiate Vis.js --- */
+  /* --- Prepare Items Array --- */
+
+  const itemsArray = [];
 
   myTBody.children('tr:not(".empty-row")').each(function(i){ // loop rows
     var tr = $(this);
@@ -79,7 +83,11 @@ $(function(){
     //console.log({desc});
     var category = tr.children('td[data-id="category"]').text().trim();
     //console.log({category});
+
+    var newArray = [];
   });
+
+  /* --- Initiate Vis.js --- */
 
   // DOM element where the Timeline will be attached
   var container = myVis[0];
@@ -87,9 +95,9 @@ $(function(){
 
   // Create a DataSet (allows two way data-binding)
   var items = new vis.DataSet([
-    {id: 1, content: 'item 1', start: new Date(2010,9,23),group: 1},
-    {id: 2, content: 'item 2', start: new Date(2011,9,23), end: new Date(2012,9,23),group: 1},
-    {id: 3, content: 'item 3', start: new Date(2013,9,23), type: 'point',group: 2}
+    {id: 1, content: 'item 1', start: new Date(2010,9,23), group: 1},
+    {id: 2, content: 'item 2', start: new Date(2011,9,23), end: new Date(2012,9,23), group: 1},
+    {id: 3, content: 'item 3', start: new Date(2013,9,23), type: 'point', group: 2}
   ]);
   console.log({items});
 
